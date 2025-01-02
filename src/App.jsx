44f2e14986ad4182d54cce8e656aac3f2450e3ac
Soundharya S <soundharya@ecappz.in>
2025-01-02 18:50:55 +0530

    Study Spouse Form

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import StudyPermitExtension from "./components/StudyPermitExtension";
 import StudyVisa from "./components/StudyVisa";
 import StudyWorkPermitExtension from "./components/StudyWorkPermitExtension";
+import StudySpouse from "./components/StudySpouse";
 
 const App = () => {
   return (
     <>
-      <StudyWorkPermitExtension />
+      <StudySpouse />
     </>
   );
 };
